31ba424add38337f605088c79f6e05ef136285a4
hosoo123 <monhjargalhosbat@gmail.com>
2026-06-08 12:33:43 +0800

    1

--- app/_components/list.js
+++ app/_components/list.js
@@ -1,8 +1,9 @@
-export const List = () => {
+export const List = (props) => {
   return (
     <div className="w-full flex justify-between bg-[#F3F4F6] p-4 rounded-lg">
       <div className="text-black flex gap-2.5 items-center ">
-        <input className="cursor-pointer w-5 h-5" type="checkbox" value={""} />
+        <input className="cursor-pointer w-5 h-5" type="checkbox" />
+        {props.todo}
       </div>
       <button className="text-[#ef4444] cursor-pointer rounded-lg bg-[#fef2f2] w-[67] h-[30]">
         Delete
@@ -10,3 +11,8 @@ export const List = () => {
     </div>
   );
 };
+// {
+//   todos.map((todo) => {
+//     todo;
+//   });
+// }
--- app/page.js
+++ app/page.js
@@ -10,16 +10,23 @@ export default function Home() {
   const Btn = () => {
     setIndex(index + 1);
   };
-  const [select, setSelected] = useState("");
+  const [select, setSelected] = useState("All");
   const firstHandle = () => {
-    setSelected = "All";
+    setSelected("All");
   };
   const secondHandle = () => {
-    setSelected = "Active";
+    setSelected("Active");
   };
   const thirdHandle = () => {
-    setSelected = "Completed";
+    setSelected("Completed");
   };
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleAdd = () => {
+    setTodos([...todos, value]);
+    setValue("");
+  };
+
   return (
     <div className="bg-white-900 drop-shadow-1g max-w-full flex justify-center pt-[60]">
       <div className=" w-[377] border drop-shadow-lg flex p-6 text-center rounded-xl flex-col bg-white gap-8">
@@ -27,32 +34,61 @@ export default function Home() {
           <p className="text-black text-xl font-bold font-sans" onClick={Btn}>
             To-Do List
           </p>
-          {index}
+          Task - {index}
           <div className="flex gap-1.5">
             <input
+              value={value}
+              onChange={(event) => {
+                setValue(event.target.value);
+              }}
               id="inputContain"
               className="text-black w-full border-gray-600 border rounded-lg p-1.5 text-sm"
               placeholder="Add a new task..."
             />
-            <button className="flex w-[59] cursor-pointer text-sm h-10 text-white bg-blue-600 items-center justify-center rounded-xl">
+            <button
+              onClick={handleAdd}
+              className="flex w-[59] cursor-pointer text-sm h-10 text-white bg-blue-600 items-center justify-center rounded-xl"
+            >
               Add
             </button>
           </div>
           <div className="flex gap-1.5 h-8 text-xs">
-            <button className="text-white cursor-pointer rounded-lg bg-blue-600 w-[38]">
+            <button
+              className={`cursor-pointer rounded-lg px-3 transition-colors ${
+                select === "All"
+                  ? "bg-blue-600 text-white"
+                  : "bg-gray-300 text-black "
+              }`}
+              onClick={firstHandle}
+            >
               All
             </button>
-            <button className="text-black w-[60] rounded-lg cursor-pointer bg-gray-300">
+            <button
+              className={`cursor-pointer rounded-lg "${
+                select == "Active"
+                  ? "text-black bg-blue-600 w-[60px]"
+                  : " bg-gray-300 text-black w-[60px]"
+              } `}
+              onClick={secondHandle}
+            >
               Active
             </button>
-            <button className="text-black w-[87] cursor-pointer bg-gray-300 rounded-lg">
+            <button
+              className={`cursor-pointer rounded-lg "${
+                select == "Completed"
+                  ? "text-black bg-blue-600 w-[87px]"
+                  : " bg-gray-300 text-black w-[87px]"
+              } `}
+              onClick={thirdHandle}
+            >
               Completed
             </button>
           </div>
         </div>
         <div className="flex gap-[22px] flex-col">
-          <List />
-          <List />
+          {todos.map((item) => (
+            <List todo={item} />
+          ))}
           <hr />
           <Alert />
         </div>
